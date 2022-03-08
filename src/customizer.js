9db9e4350fc816820ce577e0aa554556d2bd857b
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-08 17:55:17 +0600

    Customizer panel in javascript and scss
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

---
+++ src/customizer.js
@@ -0,0 +1,159 @@
+import "./customizer.scss";
+
+const { api } = wp;
+
+import {
+	Panel,
+	PanelBody,
+	Placeholder,
+	SelectControl,
+	Spinner,
+	TextControl,
+	ToggleControl,
+} from "@wordpress/components";
+
+import { Fragment, render, Component } from "@wordpress/element";
+
+import { __ } from "@wordpress/i18n";
+
+const { customize } = wp;
+
+class App extends Component {
+	constructor() {
+		super(...arguments);
+
+		this.state = {
+			customizerSelect: "",
+			customizerText: "",
+			customizerText2: "",
+			customizerToggle: false,
+			isAPILoaded: false,
+		};
+	}
+
+	componentDidMount() {
+		api.loadPromise.then(() => {
+			this.settings = new api.models.Settings();
+
+			const { isAPILoaded } = this.state;
+
+			if (isAPILoaded === false) {
+				this.settings.fetch().then((response) => {
+					this.setState({
+						customizerSelect: WPGutenCustomizer["wp_guten_customizer_select"],
+						customizerText: WPGutenCustomizer["wp_guten_customizer_text"],
+						customizerText2: response["wp_guten_customizer_text_2"],
+						customizerToggle: Boolean(response["wp_guten_customizer_toggle"]),
+						isAPILoaded: true,
+					});
+				});
+			}
+		});
+	}
+
+	render() {
+		const {
+			customizerSelect,
+			customizerText,
+			customizerText2,
+			customizerToggle,
+			isAPILoaded,
+		} = this.state;
+
+		if (!isAPILoaded) {
+			return (
+				<Placeholder>
+					<Spinner />
+				</Placeholder>
+			);
+		}
+
+		return (
+			<Fragment>
+				<div className="wp-guten-customizer__main">
+					<Panel>
+						<PanelBody
+							title={__("Panel Body One", "wp-guten-options")}
+							icon="admin-plugins"
+						>
+							<SelectControl
+								help={__("An example dropdown field.", "wp-guten-options")}
+								label={__("Example Select", "wp-guten-options")}
+								onChange={(customizerSelect) => {
+									this.setState({ customizerSelect });
+									customize.value("wp_guten_customizer_select")(
+										customizerSelect
+									);
+								}}
+								options={[
+									{
+										label: __("Please Select...", "wp-guten-options"),
+										value: "",
+									},
+									{
+										label: __("Option 1", "wp-guten-options"),
+										value: "option-1",
+									},
+									{
+										label: __("Option 2", "wp-guten-options"),
+										value: "option-2",
+									},
+								]}
+								value={customizerSelect}
+							/>
+						</PanelBody>
+						<PanelBody
+							title={__("Panel Body Two", "wp-guten-options")}
+							icon="admin-plugins"
+						>
+							<TextControl
+								help={__("This is an example text field.", "wp-guten-options")}
+								label={__("Example Text", "wp-guten-options")}
+								onChange={(customizerText) => {
+									this.setState({ customizerText });
+									customize.value("wp_guten_customizer_text")(customizerText);
+								}}
+								value={customizerText}
+							/>
+						</PanelBody>
+						<PanelBody
+							title={__("Panel Body Three", "wp-guten-options")}
+							icon="admin-plugins"
+						>
+							<TextControl
+								help={__(
+									"Use PanelRow to place controls inline.",
+									"wp-guten-options"
+								)}
+								label={__("Example Text 2", "wp-guten-options")}
+								onChange={(customizerText2) => {
+									this.setState({ customizerText2 });
+									customize.value("wp_guten_customizer_text_2")(
+										customizerText2
+									);
+								}}
+								value={customizerText2}
+							/>
+						</PanelBody>
+						<PanelBody
+							title={__("Panel Body Four", "wp-guten-options")}
+							icon="admin-plugins"
+						>
+							<ToggleControl
+								checked={customizerToggle}
+								help={__("An example toggle.", "wp-guten-options")}
+								label={__("Example Toggle", "wp-guten-options")}
+								onChange={(customizerToggle) => {
+									this.setState({ customizerToggle });
+									customize.value("wp_guten_customizer_toggle")(
+										customizerToggle
+									);
+								}}
+							/>
+						</PanelBody>
+					</Panel>
+				</div>
+			</Fragment>
+		);
+	}
+}
